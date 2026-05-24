39947728f1c9ef13a7f5da9b24369383085e4a7e
Andy Alexander Inampues Posso <tu-correo@ejemplo.com>
2026-05-24 09:29:28 -0500

    Limpieza final de archivos

---
+++ app.js
@@ -0,0 +1,5 @@
+// Mensaje de bienvenida al cargar la página
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portafolio cargado correctamente");
+    alert("¡Bienvenido a mi sitio profesional, soy Andy!");
+});
